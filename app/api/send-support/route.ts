d732c6272e8fcceeb84738a7d8a062cdf2324238
John1701708 <markgreen7383@gmail.com>
2026-09-09 15:46:00 +0500

    Update route.ts

--- app/api/send-support/route.ts
+++ app/api/send-support/route.ts
@@ -46,7 +46,7 @@ export async function POST(req: NextRequest) {
     // Pehli email — markgreen ke account se markgreen ko
     const sendMark = resendMark.emails.send({
       from: "TempNova <onboarding@resend.dev>",
-      to: ["markgreen7383@gmail.com"],
+      to: ["v"],
       subject: `TempNova Support — ${tempEmail || "Anonymous"}`,
       html: htmlContent,
       text: textContent,
